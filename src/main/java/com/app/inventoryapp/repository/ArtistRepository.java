d4d10d71aff9ea1f129803c7c368d5fa8fda0eb2
Megan Kociscak <megankociscak@gmail.com>
2022-02-03 04:25:43 -0500

    Repositories added (3)

---
+++ src/main/java/com/app/inventoryapp/repository/ArtistRepository.java
@@ -0,0 +1,11 @@
+package com.app.inventoryapp.repository;
+
+import com.app.inventoryapp.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+
+public interface ArtistRepository extends JpaRepository<Artist, Long>{
+}
---
+++ src/main/java/com/app/inventoryapp/repository/TitleRepository.java
@@ -0,0 +1,11 @@
+package com.app.inventoryapp.repository;
+
+import com.app.inventoryapp.model.Title;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+
+public interface TitleRepository extends JpaRepository<Title, Long>{
+}
