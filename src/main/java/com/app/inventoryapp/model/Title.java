7045466b4e5b23476a64ccffeffd9a4443894243
Megan Kociscak <megankociscak@gmail.com>
2022-02-03 04:37:02 -0500

    Title model added

---
+++ src/main/java/com/app/inventoryapp/model/Title.java
@@ -0,0 +1,66 @@
+package com.app.inventoryapp.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Title {
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private Integer year;
+
+    
+    public Title() {
+    }
+
+    public Title(Long id, String title, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Title{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+
+}
